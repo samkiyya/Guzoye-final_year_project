d8987734a0899e6e6f47c56b63885abd314aec64
samkiyya <samuelkiyya2012@gmail.com>
2024-06-19 22:03:48 +0300

    package updated

---
+++ server/src/middleware/validate.ts
@@ -0,0 +1,41 @@
+import { NextFunction, Request, Response } from "express";
+
+export function validate(req: Request, res: Response, next: NextFunction) {
+  const { email, username, password } = req.body;
+
+  function validEmail(userEmail: string) {
+    // regex for checking if email is valid
+    return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(userEmail);
+  }
+
+  function validUsername(username: string) {
+    // username should be between 3 and 20 characters
+    return username.length > 3 && username.length < 20;
+  }
+
+  if (req.path === "/register") {
+    console.log(!email.length);
+    if (![email, username, password].every(Boolean)) {
+      return res
+        .status(401)
+        .json({ error: "Missing Credentials", success: false });
+    } else if (!validEmail(email)) {
+      return res.status(401).json({ error: "Invalid Email", success: false });
+    } else if (!validUsername(username)) {
+      return res.status(401).json({
+        error: "Invalid Username. Should be between 3 and 20 characters",
+        success: false,
+      });
+    }
+  } else if (req.path === "/login") {
+    if (![email, password].every(Boolean)) {
+      return res
+        .status(401)
+        .json({ error: "Missing Credentials", success: false });
+    } else if (!validEmail(email)) {
+      return res.status(401).json({ error: "Invalid Email", success: false });
+    }
+  }
+
+  next();
+}
