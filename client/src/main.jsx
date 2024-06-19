01cff6c9de5b0ab5b8c849bfed2d841d2b48d41e
samkiyya <samuelkiyya2012@gmail.com>
2024-06-20 00:00:41 +0300

    authprovider updated

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,17 +5,14 @@ import "./index.css";
 import { Provider } from "react-redux";
 import { persistor, store } from "./redux/store.js";
 import ThemeProvider from "./components/ThemeProvider.jsx";
-import { AuthProvider } from "./context/AuthContext";
 import { PersistGate } from "redux-persist/integration/react";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <ThemeProvider>
-          <AuthProvider>
             {" "}
             <App />
-          </AuthProvider>
         </ThemeProvider>
       </PersistGate>
     </Provider>
